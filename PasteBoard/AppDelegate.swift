6aabb628bb9007db47717fbb31e945d00fd8b698
Steven J. Selcuk <stevenjselcuk@gmail.com>
2021-03-10 19:54:50 +0300

    Initial Commit

---
+++ PasteBoard/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PasteBoard
+//
+//  Created by Steven J. Selcuk on 10.03.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PasteBoard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PasteBoard
+//
+//  Created by Steven J. Selcuk on 10.03.2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
